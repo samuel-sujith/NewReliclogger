4693f0863930b04e4dcf461e59abfed3323146be
Sujith Samuel <Sujith.Samuel@ericsson.com>
2022-03-26 18:20:24 +0100

    code cleanup and error changes

--- platform/app/client.go
+++ platform/app/client.go
@@ -36,7 +36,7 @@ func (S *Stub) GitcheckUp(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("http://" + S.ToxiProxy.Listen)
 	fmt.Println("response from gitup", resp)
 	if err == nil {
-		S.GitDownHits.Increment()
+		S.GitUpHits.Increment()
 	} else {
 		S.GitDownHits.Increment()
 	}
@@ -52,16 +52,14 @@ func (S *Stub) GitcheckDown(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("http://" + S.ToxiProxy.Listen)
 	fmt.Println("response from gitdown", resp)
 
-	if err != nil {
-		S.GitDownHits.Increment()
-	} else if resp.StatusCode != 200 {
+	if err != nil || resp.StatusCode != 200 {
 		S.GitDownHits.Increment()
 	}
 }
 
 func (S *Stub) RedisBackendSlow(w http.ResponseWriter, r *http.Request) {
 
-	S.RedisProxy.AddToxic("latency_downstream", "latency", "", 1, toxiproxy.Attributes{
+	S.RedisProxy.AddToxic("latency_downstream", "latency", "downstream", 1, toxiproxy.Attributes{
 		"latency": 1000,
 	}) //This adds latency to the Redis connect request
 	defer S.RedisProxy.RemoveToxic("latency_downstream")
